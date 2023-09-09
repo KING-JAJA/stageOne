30bedbaa47e696fd95c9f089ab6861518ce8e1e0
KING-JAJA <104613683+KING-JAJA@users.noreply.github.com>
2023-09-09 16:07:38 +0100

    correction to the utc time on app.js

--- app.js
+++ app.js
@@ -7,30 +7,32 @@ app.use(bodyparser.json());
 
 let localDate = new Date();
 
-let utcTime = () => {
-    // Get the current UTC time in milliseconds
-    let currentUTCTime = Date.now();
+// let utcTime = () => {
+//     // Get the current UTC time in milliseconds
+//     let currentUTCTime = Date.now();
   
-    // Get the UTC time in seconds (rounded down to the nearest second)
-    let currentUTCSeconds = Math.floor(currentUTCTime / 1000);
+//     // Get the UTC time in seconds (rounded down to the nearest second)
+//     let currentUTCSeconds = Math.floor(currentUTCTime / 1000);
   
-    // Get the local time in seconds (rounded down to the nearest second)
-    let currentLocalSeconds = Math.floor(Date.now() / 1000);
+//     // Get the local time in seconds (rounded down to the nearest second)
+//     let currentLocalSeconds = Math.floor(Date.now() / 1000);
   
-    // Calculate the time difference in seconds
-    let timeDifference = currentLocalSeconds - currentUTCSeconds;
+//     // Calculate the time difference in seconds
+//     let timeDifference = currentLocalSeconds - currentUTCSeconds;
 
-    if (Math.abs(timeDifference) <= 2) {
-        // The current UTC time is within +/- 2 seconds of accuracy
-        return new Date(currentUTCTime);
-    }
-}
+//     if (Math.abs(timeDifference) <= 2) {
+//         // The current UTC time is within +/- 2 seconds of accuracy
+//         return new Date(currentUTCTime);
+//     }
+// }
+
+let utc = new Date().toISOString().slice(0, 19) + "Z";
 
 app.get("/", (req, res) => {
     res.json({
         "slack_name": "SamuelJaja",
         "current_day": localDate.toLocaleDateString('en-EN', { weekday: 'long' }),
-        "utc_time": utcTime(),
+        "utc_time": utc,
         "track": "backend",
         "github_file_url": "https://github.com/KING-JAJA/stageOne/blob/main/app.js",
         "github_repo_url": "https://github.com/KING-JAJA/stageOne",
@@ -43,7 +45,7 @@ app.get('/api', (req, res) => {
     res.json({
         "slack_name": `${slack_name || "SamuelJaja"}`,
         "current_day": localDate.toLocaleDateString('en-EN', { weekday: 'long' }),
-        "utc_time": utcTime(),
+        "utc_time": utc,
         "track": `${track || "backend"}`,
         "github_file_url": "https://github.com/KING-JAJA/stageOne/blob/main/app.js",
         "github_repo_url": "https://github.com/KING-JAJA/stageOne",
